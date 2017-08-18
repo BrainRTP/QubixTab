dedc76abee2801a543d1f50ecf11de8075761d5e
BuildTools <unconfigured@null.spigotmc.org>
2017-08-18 23:12:04 +0300

    NV

--- src/main/java/ru/brainrtp/core/Main.java
+++ src/main/java/ru/brainrtp/core/Main.java
@@ -1,7 +1,10 @@
 package ru.brainrtp.core;
 
+import java.io.IOException;
 import java.util.logging.Logger;
 import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 import ru.brainrtp.core.chat.Chat;
@@ -40,6 +43,41 @@ public final class Main extends JavaPlugin {
     }
 
 
+    // | COMMANDS | ///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("qubix")) {
+            switch(args[0]){
+                case "help": sender.sendMessage(ChatColor.YELLOW+"QubixCore " + ChatColor.DARK_GRAY+"v"+pdfFile.getVersion()); break;
+                default: sender.sendMessage(ChatColor.RED + "Неизвестная команда.");
+            }
+
+
+            if(args[0].equalsIgnoreCase("test")){
+                //Player player = (Player) sender;
+                //player.sendTitle("Ты че", "Сука", 0, 3, 0);
+                //player.setPlayerListName(player.getDisplayName().replace("&", "§"));
+                //TitleAPI.sendTabTitle(player, "Header", "Footer");
+                //sender.setScoreboard(tab.);
+                return false;
+            }
+            else if(args[0].equalsIgnoreCase("reload")){
+                /*try {
+                    loadConfig();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }*/
+                sender.sendMessage(ChatColor.GREEN+"Конфиг перезагружен.");
+                return false;
+            }
+
+        }
+        return false;
+    }
+
+
+
     // | OTHER | ///////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
@@ -47,6 +85,6 @@ public final class Main extends JavaPlugin {
         return ChatColor.translateAlternateColorCodes('&', string);
     }
 
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////
 }
